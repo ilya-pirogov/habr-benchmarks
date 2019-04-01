a10362232d830de7e266550794c5c86fb1aa65ae
Ilya Pirogov <ilya@steam.me>
2019-04-01 12:53:13 -0500

    Remove debug info from go bins

--- compile-all.sh
+++ compile-all.sh
@@ -9,9 +9,9 @@ g++ -O3 -march=native -o ./bin/cpp-multi-gcc -pthread ./cpp-multi.cpp
 
 ghc -O3 -o ./bin/haskell-single ./haskell-single.hs
 
-go build -o ./bin/go-single ./go-single.go
-go build -o ./bin/go-multi-1 ./go-multi-1.go
-go build -o ./bin/go-multi-2 ./go-multi-2.go
+go build -o ./bin/go-single -ldflags "-s -w" ./go-single.go
+go build -o ./bin/go-multi-1 -ldflags "-s -w" ./go-multi-1.go
+go build -o ./bin/go-multi-2 -ldflags "-s -w" ./go-multi-2.go
 
 # have no idea how to solve dependencies without project
 # rustc -o ./bin/rust-single ./rust-single.rs -- error[E0432]: unresolved import `clap`
